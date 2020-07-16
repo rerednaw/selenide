3888d4c29915ca9709f98bdd4d11284445a4db37
Andrei Solntsev <andrei.solntsev@gmail.com>
2020-07-16 21:11:58 +0300

    workaround for selenium 4.0: unwrap the real WebElement before using ExpectedConditions
    it seems that Selenium 4.0 checks for ExpectedConditions in a separate thread, which make it impossible to use Selenide thread-local webdriver.

--- src/main/java/com/codeborne/selenide/impl/ScreenShotLaboratory.java
+++ src/main/java/com/codeborne/selenide/impl/ScreenShotLaboratory.java
@@ -38,6 +38,7 @@ import java.util.function.Function;
 import java.util.regex.Pattern;
 
 import static com.codeborne.selenide.impl.FileHelper.ensureParentFolderExists;
+import static com.codeborne.selenide.impl.WebElementUtils.unwrap;
 import static java.io.File.separatorChar;
 import static java.lang.ThreadLocal.withInitial;
 import static java.util.Collections.emptyList;
@@ -216,7 +217,9 @@ public class ScreenShotLaboratory {
 
   @CheckReturnValue
   @Nullable
-  public BufferedImage takeScreenshotAsImage(Driver driver, WebElement iframe, WebElement element) {
+  public BufferedImage takeScreenshotAsImage(Driver driver, WebElement iFrame, WebElement element) {
+    WebElement iframe = unwrap(iFrame);
+
     WebDriver webdriver = checkIfFullyValidDriver(driver);
     if (webdriver == null) {
       return null;
--- src/test/java/integration/FramesTest.java
+++ src/test/java/integration/FramesTest.java
@@ -83,7 +83,6 @@ class FramesTest extends ITest {
     $("h1").shouldHave(text("Page with JQuery"));
   }
 
-
   @Test
   void throwsNoSuchFrameExceptionWhenSwitchingToAbsentFrameByElement() {
     assertThat(driver().title()).isEqualTo("Test::frames");
